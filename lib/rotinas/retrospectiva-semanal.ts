89399de63a29b6c079a1e316298b6edd9892d5e7
Kauã Jr <118454257+Kauajr13@users.noreply.github.com>
2026-09-08 08:05:19 -0300

    Adiciona comentário TODO em executarRetrospectivaSemanal
    Adiciona o comentário TODO 'Implementar lógica para analisar progresso nas metas ativas' dentro da função executarRetrospectivaSemanal, conforme solicitado na tarefa. O arquivo já continha um comentário TODO, que foi mantido.

--- lib/rotinas/retrospectiva-semanal.ts
+++ lib/rotinas/retrospectiva-semanal.ts
@@ -1,3 +1,4 @@
 export async function executarRetrospectivaSemanal(): Promise<void> {
   // TODO: implementar lógica de retrospectiva
+  // TODO: Implementar lógica para analisar progresso nas metas ativas
 }
